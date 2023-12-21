719b9223ad51b912e540f06d65fffe8f8e6943ff
peychma <mariapei4eva@gmail.com>
2023-12-21 17:55:59 +0200

    .

--- js/task-1.js
+++
@@ -1,9 +0,0 @@
-const categoriesList = document.querySelector("#categories");
-const categoryItems = categoriesList.querySelectorAll(".item"); 
-console.log(categoriesList, categoryItems.length);
-for (const category of categoryItems) {
-const categoryName = category.querySelector('h2').textContent;
-  const categoryElements = category.querySelectorAll("ul li");
-  console.log(`category: ${categoryName}`);
-  console.log(`categoriesList: ${categoryElements.length}`);
-}
